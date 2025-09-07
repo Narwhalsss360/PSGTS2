3204e11c1ae3d2a3832ae3dd5b647ab55d66bdc0
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-09-06 22:04:00 -0400

    Added vJoy

--- connector/Program.cs
+++ connector/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Connector;
+using vJoyInterfaceWrap;
 
 Console.Write("Discovering...\r");
 if (PSGTS2.Discover() is not PSGTS2 psgts2)
@@ -7,7 +8,78 @@ if (PSGTS2.Discover() is not PSGTS2 psgts2)
     return;
 }
 Console.WriteLine($"PSGTS2 Guitar on port {psgts2.Port.PortName}");
+vJoy vJoy = new();
 
+if (!vJoy.vJoyEnabled())
+{
+    Console.Error.WriteLine("vJoy not installed, which is required.");
+    return;
+}
+
+uint joyId = 1;
+
+if (args.Length > 1 && !uint.TryParse(args[1], out joyId))
+{
+    Console.Error.WriteLine("Argument must be joy id (1-16)");
+    Environment.Exit(1);
+    return;
+}
+
+uint libraryVersion = 0;
+uint driverVersion = 0;
+if (!vJoy.DriverMatch(ref libraryVersion, ref driverVersion))
+{
+    Console.Error.WriteLine($"\x1B[33mWARNING\x1B[0m: Library-Driver mismatch.");
+}
+
+switch (vJoy.GetVJDStatus(joyId))
+{
+    case VjdStat.VJD_STAT_OWN:
+    case VjdStat.VJD_STAT_FREE:
+        break;
+    case VjdStat.VJD_STAT_BUSY:
+        Console.Error.WriteLine($"vJoy error: {joyId} is already owned by another feeder.");
+        Environment.Exit(1);
+        return;
+    case VjdStat.VJD_STAT_MISS:
+        Console.Error.WriteLine($"vJoy error: {joyId} is disabled.");
+        Environment.Exit(1);
+        return;
+    case VjdStat.VJD_STAT_UNKN:
+        Console.Error.WriteLine($"vJoy unknown error for {joyId}.");
+        Environment.Exit(1);
+        return;
+    default:
+        break;
+}
+
+if (!vJoy.GetVJDAxisExist(joyId, HID_USAGES.HID_USAGE_X))
+{
+    Console.Error.WriteLine("Re-configure vJoy, x-axis is required.");
+    Environment.Exit(1);
+    return;
+}
+
+if (vJoy.GetVJDButtonNumber(joyId) < 13)
+{
+    Console.Error.WriteLine("Re-configure vJoy, 13 buttons are required.");
+    Environment.Exit(1);
+    return;
+}
+
+if (!vJoy.AcquireVJD(joyId))
+{
+    Console.Error.WriteLine($"Failed to aquire device {joyId}.");
+    Environment.Exit(1);
+    return;
+}
+
+long xMin = 0;
+long xMax = 0;
+vJoy.GetVJDAxisMin(joyId, HID_USAGES.HID_USAGE_X, ref xMin);
+vJoy.GetVJDAxisMax(joyId, HID_USAGES.HID_USAGE_X, ref xMax);
+
+vJoy.JoystickState joyState = new();
 TimeSpan consoleRefreshRate = TimeSpan.FromMilliseconds(30);
 string[] dioNames = Enum.GetNames<PSGTS2.State.DIO>();
 string[] aioNames = Enum.GetNames<PSGTS2.State.AIO>();
@@ -16,6 +88,23 @@ DateTime lastPrint = DateTime.MinValue;
 DateTime lastFlush = DateTime.MinValue;
 psgts2.StateReady += (sender, e) =>
 {
+    joyState.AxisX = (int)((xMax - xMin) * psgts2.CurrentState.Get(PSGTS2.State.AIO.Whammy) + xMin);
+    joyState.Buttons = 0;
+
+    for (int i = 0; i < PSGTS2.State.DIO_COUNT; i++)
+    {
+        if (!psgts2.CurrentState.Dio[i])
+            continue;
+        joyState.Buttons |= (uint)(1 << i);
+    }
+
+    if (!vJoy.UpdateVJD(joyId, ref joyState))
+    {
+        Console.Error.WriteLine($"\x1B[31mERROR\x1B[0m: Joystick update error.");
+        Environment.Exit(1);
+        return;
+    }
+
     if (DateTime.Now - lastPrint < consoleRefreshRate || e.Flushed)
         return;
 
